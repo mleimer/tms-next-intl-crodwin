4a15630c0c2ab2724fbc657b351c34140eaf16de
Martin Leimer <martin.leimer@ergon.ch>
2024-02-01 14:35:14 +0100

    Initialize Next.js app with next-intl

---
+++ tms-next-intl-crowdin/src/app/[locale]/page.tsx
@@ -0,0 +1,16 @@
+import {useTranslations} from 'next-intl';
+
+export default function Index() {
+    const t = useTranslations('Index');
+    return (
+        <div>
+            <h1>{t('title')}</h1>
+            <div>ICU 0: {t('icu', {count: 0})}</div>
+            <div>ICU 1: {t('icu', {count: 1})}</div>
+            <div>ICU other: {t('icu', {count: 2})}</div>
+            <div>Rich text: {t.rich('richText', {
+                b: (chunks) => <b>{chunks}</b>,
+                i: (chunks) => <i>{chunks}</i>
+            })}</div>
+        </div>);
+}
